be78df319beb5ba0a0d5f6927b3646104e3bc53b
Racskó Ádám Ákos <h376536@stud.u-szeged.hu>
2024-10-25 15:05:47 +0200

    kosár test

--- Frontend/src/pages/Cart.jsx
+++ Frontend/src/pages/Cart.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import React, { useContext } from "react";
+import './CSS/Cart.css'
+import { ShopContext } from "../context/ShopContext";
+import Item from "../Components/Items/Item";
+
+
 
 const Cart = () => {
+  const { ital_product } = useContext(ShopContext);
   return (
-    <div>Cart</div>
+    <div className='cart'>
+      <div className="cartContainer">
+        <div className="cartItems">
+          <table>
+            <thead>
+              <th>Termék</th>
+              <th>Leírás</th>
+              <th>Ár</th>
+              <th>Mennyiség</th>
+              <th>Összeg</th>
+            </thead>
+            <tbody>
+            {ital_product.slice(0,3).map((item,i) => {
+            return (
+              <tr>
+                <td><img src={item.image} alt="" /></td>
+                <td>
+                  <b>{item.name}</b><br/>
+                  {item.volume}
+                </td>
+                <td>{item.price} Ft</td>
+                <td><input type="number" value='1'/></td>
+                <td>{item.price} Ft</td>
+              </tr>
+            );
+      })}
+            </tbody>
+          </table>
+        </div>
+        <div className="cartOverview">
+          <h2>Végösszeg</h2>
+          1234 Ft
+          <button>Tovább a fizetéshez</button>
+        </div>
+      </div>
+    </div>
   )
 }
 

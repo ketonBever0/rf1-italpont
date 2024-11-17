a59266dce0c14c81038392271501918ccda0f35c
K-N.Mate <h372195@stud.u-szeged.hu>
2024-11-17 22:09:40 +0100

    / jelek kiszedése

--- Frontend/src/pages/Admin.jsx
+++ Frontend/src/pages/Admin.jsx
@@ -3,30 +3,24 @@ import Sidebar from "../components/Sidebar/Sidebar";
 
 
 const Admin = () =>  {
-  //const currentUser = JSON.parse(window.localStorage.getItem("currentUser"));
-  //const isAdmin = currentUser != null ? currentUser.role === "ADMIN" : false;
+  const currentUser = JSON.parse(window.localStorage.getItem("currentUser"));
+  const isAdmin = currentUser != null ? currentUser.role === "ADMIN" : false;
 
-  //if (!isAdmin) {
-   // window.location.href = "/";
-   // return null;
- // }
-  //function logOut() {
-   // window.localStorage.removeItem("token");
-   // window.localStorage.removeItem("currentUser");
+  if (!isAdmin) {
+   window.location.href = "/";
+    return null;
+  }
+  function logOut() {
+    window.localStorage.removeItem("token");
+    window.localStorage.removeItem("currentUser");
 
-    //window.location.href = "/";
-  //}
+    window.location.href = "/";
+  }
   return (
     <div>
-<<<<<<< Updated upstream
-      <Sidebar page="admin" />
-=======
       <Sidebar />
       <div>Admin</div>
-      <div>
-        <a href="Tables.jsx">Raktárak</a>
-      </div>
->>>>>>> Stashed changes
+      
       <div>
        
         <button onClick={logOut}>Kijelentkezés</button>

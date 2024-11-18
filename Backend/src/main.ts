5505e77fd9f628b0186c7c022818d2d6f574d303
Kurucz Laci <lacikurucz09@gmail.com>
2024-11-18 23:55:16 +0100

    Update main.ts

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from "@nestjs/common";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.setGlobalPrefix("api");
+  // app.setGlobalPrefix("api");
   app.enableCors({
     origin: true,
     credentials: true,

0747f1d82b38d76a91076ec229d5a2868aa0aa62
Kurucz Laci <lacikurucz09@gmail.com>
2024-11-26 15:18:53 +0100

    Update order.controller.ts

--- Backend/src/order/order.controller.ts
+++ Backend/src/order/order.controller.ts
@@ -1,68 +1,74 @@
 import {
-    BadRequestException,
-    Body,
-    Controller,
-    Delete,
-    Get,
-    NotFoundException,
-    Param,
-    Patch,
-    Post,
-    Req,
-    Res,
-    UploadedFiles,
-    UseGuards,
-    UseInterceptors,
-  } from "@nestjs/common";
-  import { Response } from "express";
-  import { AuthGuard } from "src/auth/guard/auth.guard";
-  import { RoleGuard } from "src/auth/guard/role.guard";
-  import { Roles, User } from "src/auth/auth.decorator";
-  import { FilesInterceptor } from "@nestjs/platform-express";
-  import { diskStorage } from "multer";
-  import { extname, join } from "path";
-  import * as fs from "fs";
+  BadRequestException,
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Patch,
+  Post,
+  Req,
+  Res,
+  UploadedFiles,
+  UseGuards,
+  UseInterceptors,
+} from "@nestjs/common";
+import { Response } from "express";
+import { AuthGuard } from "src/auth/guard/auth.guard";
+import { RoleGuard } from "src/auth/guard/role.guard";
+import { Roles, User } from "src/auth/auth.decorator";
+import { FilesInterceptor } from "@nestjs/platform-express";
+import { diskStorage } from "multer";
+import { extname, join } from "path";
+import * as fs from "fs";
 import { OrderService } from "./order.service";
 import { AddOrderDTO, UpdateOrderDTO } from "./dto/order.dto";
 import { json } from "stream/consumers";
 import { User as UserModel } from "@prisma/client";
 
 @Controller("/order")
-export class OrderController{
-    constructor(private readonly orderService: OrderService){}
+export class OrderController {
+  constructor(private readonly orderService: OrderService) {}
 
-    @Get("/all")
-    async getOrders(){
-        return await this.orderService.getOrders();
+  @Get("/all")
+  async getOrders() {
+    return await this.orderService.getOrders();
+  }
+
+  @UseGuards(AuthGuard)
+  @Get("/get/:id")
+  async getOne(@Param("id") id: string) {
+    return await this.orderService.getOneOrder(parseInt(id));
+  }
+
+  @UseGuards(AuthGuard)
+  @Post("/add")
+  async addOrder(
+    @User() user: any,
+    @Body() dto: AddOrderDTO,
+    @Res() res: Response,
+  ) {
+    console.log(user);
+    const order = await this.orderService
+      .addOrder(user.sub, dto)
+      .catch((e) => console.log(e));
+    if (order) {
+      res.status(200).json({ message: "Termék felvéve", order });
+    } else {
+      throw new NotFoundException();
     }
+  }
 
-    @UseGuards(AuthGuard)
-    @Get("/get/:id")
-    async getOne(@Param("id") id: string) {
-        return await this.orderService.getOneOrder(parseInt(id));
-    }
+  @UseGuards(AuthGuard, RoleGuard)
+  @Roles("ADMIN", "MODERATOR")
+  @Delete("/delete/:id")
+  async deleteOrder(@Param("id") id: string, @Res() res: Response) {
+    const result = await this.orderService.deleteOrder(parseInt(id));
+    res.status(200).json(result);
+  }
 
-    @UseGuards(AuthGuard)
-    @Post("/add")
-    async addOrder(@User() user: any, @Body() dto: AddOrderDTO, @Res() res: Response){
-      console.log(user)
-        const order = await this.orderService.addOrder(user.sub, dto).catch(e => console.log(e));
-        if(order){
-            res.status(200).json({ message: "Termék felvéve", order })
-        }else {
-            throw new NotFoundException();
-          }
-    }
-
-    @UseGuards(AuthGuard, RoleGuard)
-    @Roles("ADMIN", "MODERATOR")
-    @Delete("/delete/:id")
-    async deleteOrder(@Param("id") id: string, @Res() res: Response){
-        const result = await this.orderService.deleteOrder(parseInt(id));
-        res.status(200).json(result);
-    }
-
-    @UseGuards(AuthGuard, RoleGuard)
+  @UseGuards(AuthGuard, RoleGuard)
   @Roles("ADMIN", "MODERATOR")
   @Patch("/update/:id")
   async updateProduct(
@@ -75,15 +81,20 @@ export class OrderController{
   }
 
   @Post("/addproducttoorder")
-  async addProductToOrder(@Body() body: {productId: number, orderId: number, quantity: number}){
-    await this.orderService.addProductToOrder(body.quantity, body.orderId, body.productId);
-    return {Message:"Termék hozzáadva a rendeléshez"}
+  async addProductToOrder(
+    @Body() body: { productId: number; orderId: number; quantity: number },
+  ) {
+    await this.orderService.addProductToOrder(
+      body.quantity,
+      body.orderId,
+      body.productId,
+    );
+    return { message: "Termék hozzáadva a rendeléshez" };
   }
 
   @Delete("/deleteproductfromorder/:id")
-  async deletaProductFromOrder(@Param("id") id: string){
-        await this.orderService.deleteProductFromOrder(parseInt(id));
-        return {Message:"Termék eltávolítva a rendelésből"}
+  async deletaProductFromOrder(@Param("id") id: string) {
+    await this.orderService.deleteProductFromOrder(parseInt(id));
+    return { message: "Termék eltávolítva a rendelésből" };
   }
 }
-

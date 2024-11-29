edf19f113d3ded85343ccd628900196acd8fd9cc
Szegedi Bence <benceszegedi43@gmail.com>
2024-11-29 16:07:28 +0100

    összes user kilistázása

--- Frontend/src/components/Customers/Customers.jsx
+++ Frontend/src/components/Customers/Customers.jsx
@@ -1,16 +1,47 @@
-import React from "react";
+import * as React from "react";
 import "./Customers.css";
 import Sidebar from "../Sidebar/Sidebar";
-import UsersTable from "./UsersTable";
+import AdminUsersTable from "./AdminUsersTable";
+import axios from "axios";
+import BoxUI from "@mui/material/Box";
+import CircularProgress from "@mui/material/CircularProgress";
+import { CategoryContext } from "../../context/CategoryContext";
 
 const Customers = () => {
+  const { url } = React.useContext(CategoryContext);
+  const BEARER_TOKEN = window.localStorage.getItem("token");
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [allUsers, setAllUsers] = React.useState();
+
+  React.useEffect(() => {
+    axios
+      .get(url + "auth/all", {
+        headers: {
+          Authorization: `Bearer ${BEARER_TOKEN}`,
+        },
+      })
+      .then((response) => {
+        setAllUsers(response.data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
   return (
     <div className="myorders">
       <Sidebar page="admin" />
       <div className="myorders-content">
         <h1>Felhasználók</h1>
 
-        <UsersTable />
+        {!isLoading ? (
+          <AdminUsersTable users={allUsers} />
+        ) : (
+          <BoxUI sx={{ display: "flex", alignItems: "center" }}>
+            <CircularProgress />
+          </BoxUI>
+        )}
       </div>
     </div>
   );
